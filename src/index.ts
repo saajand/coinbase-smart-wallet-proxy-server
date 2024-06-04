2d833565f6040327f5f95584b19bff06561513f0
saajand <saajanduwal2000@gmail.com>
2024-06-04 17:22:31 +0545

    Fix paymaster's response returning issue; Update dotenv import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+require("dotenv").config();
 import express, { Request, Response } from "express";
-import { paymasterClient } from "./config/paymasterConfig";
-import { willSponsor } from "./utils/sponsor";
 import cors from "cors";
-import dotenv from 'dotenv';
+import { willSponsor } from "./utils/sponsor";
+import { paymasterClient } from "./config/paymasterConfig";
 // import https from "https";
 // import fs from "fs";
 // import path from "path";
@@ -13,7 +13,6 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 const paymasterService = process.env.PAYMASTER_SERVICE_URL;
@@ -23,59 +22,64 @@ app.get("/", (req, res) => {
 });
 
 app.post("/paym", async (req: Request, res: Response) => {
-  const method = req.body.method;
-  // if (!req.body?.params?.chainId) {
-  //   return res.status(400).json({ error: "Missing required parameters" });
-  // }
-  const [userOp, entrypoint, chainId] = req.body.params;
-  console.log({ userOp, entrypoint, chainId, method });
+  try {
+    const method = req.body.method;
+    // if (!req.body?.params?.chainId) {
+    //   return res.status(400).json({ error: "Missing required parameters" });
+    // }
+    const [userOp, entrypoint, chainId] = req.body.params;
 
-  const willSponsorCheck = await willSponsor({
-    chainId: parseInt(chainId),
-    entrypoint,
-    userOp,
-  });
-  console.log("willSponsorCheck == ", willSponsorCheck);
-  if (!willSponsorCheck) {
-    return Response.json({ error: "Not a sponsorable operation" });
-  }
-
-  // if (["pm_getPaymasterStubData", "pm_getPaymasterData"].includes(method)) {
-  //   const fetchRes = await fetch(paymasterService, {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     method: "POST",
-  //     body: JSON.stringify(req.body),
-  //   });
-
-  //   console.log("fetchRes -----------------------", fetchRes);
-
-  //   console.log("fetchRes-----------", fetchRes);
-  //   return Response.json(await fetchRes.json());
-  // }
-  // return Response.json({ error: "Method not found" });
-
-  if (method === "pm_getPaymasterStubData") {
-    const result = await paymasterClient.getPaymasterStubData({
-      userOperation: userOp,
+    const willSponsorCheck = await willSponsor({
+      chainId: parseInt(chainId),
+      entrypoint,
+      userOp,
     });
-    console.log(
-      "pm_getPaymasterStubData result --------------------------",
-      result
-    );
-    return Response.json({ result });
-  } else if (method === "pm_getPaymasterData") {
-    const result = await paymasterClient.getPaymasterData({
-      userOperation: userOp,
-    });
-    console.log(
-      "pm_getPaymasterData result --------------------------",
-      result
-    );
-    return Response.json({ result });
+    console.log("willSponsorCheck == ", willSponsorCheck);
+    if (!willSponsorCheck) {
+      return res.json({ error: "Not a sponsorable operation" });
+    }
+
+    // Method 1
+    // if (["pm_getPaymasterStubData", "pm_getPaymasterData"].includes(method)) {
+    //   const fetchRes = await fetch(paymasterService, {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     method: "POST",
+    //     body: JSON.stringify(req.body),
+    //   });
+
+    //   console.log("fetchRes -----------------------", fetchRes);
+
+    //   console.log("fetchRes-----------", fetchRes);
+    //   return res.json(await fetchRes.json());
+    // }
+    // return res.json({ error: "Method not found" });
+
+    // Method 2
+    if (method === "pm_getPaymasterStubData") {
+      const result = await paymasterClient.getPaymasterStubData({
+        userOperation: userOp,
+      });
+      console.log(
+        "pm_getPaymasterStubData result --------------------------",
+        result
+      );
+      return res.json({ result });
+    } else if (method === "pm_getPaymasterData") {
+      const result = await paymasterClient.getPaymasterData({
+        userOperation: userOp,
+      });
+      console.log(
+        "pm_getPaymasterData result --------------------------",
+        result
+      );
+      return res.json({ result });
+    }
+    return res.json({ error: "Method not found" });
+  } catch (error) {
+    return res.status(500).json({ error: "Something went wrong" });
   }
-  return Response.json({ error: "Method not found" });
 });
 
 app.listen(PORT, () => {
